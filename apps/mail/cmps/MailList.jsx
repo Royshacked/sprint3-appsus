f63be44c74545f240fda08b2681adf3979d74b3b
Royshacked <roy.shacked@mail.huji.ac.il>
2024-05-21 20:09:54 +0300

    Add read class to MailList

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -2,10 +2,11 @@ import { MailPreview } from "./MailPreview.jsx";
 
 export function MailList({ mails }) {
 
+
     return <section className="mail-list">
         <ul>
             {mails.map(mail =>
-                <li key={mail.id}>
+                <li key={mail.id} className={mail.isRead ? 'read' : ''}>
                     <button className="starred-btn">&#9734;</button>
                     <MailPreview mail={mail} />
                 </li>
